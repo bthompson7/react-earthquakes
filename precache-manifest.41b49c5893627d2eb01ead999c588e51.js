5edb379e79456234feae89ca2eb30cc290033153
Ben Thompson <benjamin.philip.thompson@maine.edu>
2020-08-18 00:48:52 -0400

    Updates

---
+++ precache-manifest.41b49c5893627d2eb01ead999c588e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f3dfd976d793edafa926f541dec250",
+    "url": "/react-earthquakes/index.html"
+  },
+  {
+    "revision": "708fe1f04517152e97de",
+    "url": "/react-earthquakes/static/css/main.2352d01f.chunk.css"
+  },
+  {
+    "revision": "a58bc37beca401688f02",
+    "url": "/react-earthquakes/static/js/2.eaa3e16f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-earthquakes/static/js/2.eaa3e16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708fe1f04517152e97de",
+    "url": "/react-earthquakes/static/js/main.6f932877.chunk.js"
+  },
+  {
+    "revision": "b9da9cc86345be27d85c",
+    "url": "/react-earthquakes/static/js/runtime-main.bc117bdb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-earthquake/precache-manifest.0a55273f187bd023993a328b8383d73f.js"
+  "/react-earthquakes/precache-manifest.41b49c5893627d2eb01ead999c588e51.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-earthquake/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-earthquakes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
